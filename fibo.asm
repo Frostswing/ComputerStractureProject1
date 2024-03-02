dd36a940c639140132344be227640634fbe0e612
Frostswing <114078402+Frostswing@users.noreply.github.com>
2024-03-02 22:29:34 +0200

    finished question 1

--- fibo.asm
+++ fibo.asm
@@ -57,5 +57,45 @@ print_and_run:
 #         DO NOT MODIFY ABOVE THIS LINE         #
 #################################################	
 	
-fib: 
-	jr $ra
+fib:
+    addi $t1, $zero, 0   # $t1 = 0
+    beq $a0, $t1, fib_zero # if n == 0, jump fib_zero
+
+    addi $t1, $zero, 1   # $t1 = 1
+    beq $a0, $t1, fib_one # if n == 1, jump fib_one
+
+    # For n > 1, recursive calculation
+    addi $sp, $sp, -8    # Adjust stack 
+    sw $ra, 4($sp)       # Save return address $ra
+    sw $a0, 0($sp)       # Save current n
+
+    # Calculate F(n-1)
+    addi $a0, $a0, -1    #  n = n - 1
+    jal fib              #  F(n-1)
+    addi $sp, $sp, -4    # Adjust stack
+    sw $v0, 0($sp)       # Save F(n-1) result
+
+    lw $a0, 4($sp)       # Restore original n
+    addi $a0, $a0, -2    # Decrement n by 2 for F(n-2)
+
+    jal fib              # Recursive call for F(n-2)
+
+    lw $t0, 0($sp)       # Load F(n-1) result into $t0
+    add $v0, $v0, $t0    # F(n) = F(n-1) + F(n-2)
+
+    addi $sp, $sp, 4     # Remove F(n-1) result from stack
+    lw $ra, 4($sp)       # Restore return address $ra
+    addi $sp, $sp, 8     # Adjust stack back
+
+    jr $ra               # Return to caller
+
+fib_zero:
+    addi $v0, $zero, 0   # Return 0 for F(0)
+    jr $ra               # Return
+
+fib_one:
+    addi $v0, $zero, 1   # Return 1 for F(1)
+    jr $ra               # Return
+
+    addi $v0, $zero, 1          # Return 1 for F(1)
+    jr $ra             
