72aecd0eb58382f964f3fecd24dc155debe815b5
Frostswing <114078402+Frostswing@users.noreply.github.com>
2024-03-02 20:35:35 +0200

    init

---
+++ fibo.asm
@@ -0,0 +1,61 @@
+.data
+
+inMsg: .asciiz "Enter a number: "
+msg: .asciiz "Calculating F(n) for n = "
+fibNum: .asciiz "\nF(n) is: "
+.text
+
+main:
+
+	li $v0, 4
+	la $a0, inMsg
+	syscall
+
+	# take input from user
+	li $v0, 5
+	syscall
+	addi $a0, $v0, 0
+	
+	jal print_and_run
+	
+	# exit
+	li $v0, 10
+	syscall
+
+print_and_run:
+	addi $sp, $sp, -4
+	sw $ra, ($sp)
+	
+	add $t0, $a0, $0 
+
+	# print message
+	li $v0, 4
+	la $a0, msg
+	syscall
+
+	# take input and print to screen
+	add $a0, $t0, $0
+	li $v0, 1
+	syscall
+
+	jal fib
+
+	addi $a1, $v0, 0
+	li $v0, 4
+	la $a0, fibNum
+	syscall
+
+	li $v0, 1
+	addi $a0, $a1, 0
+	syscall
+	
+	lw $ra, ($sp)
+	addi $sp, $sp, 4
+	jr $ra
+
+#################################################
+#         DO NOT MODIFY ABOVE THIS LINE         #
+#################################################	
+	
+fib: 
+	jr $ra
